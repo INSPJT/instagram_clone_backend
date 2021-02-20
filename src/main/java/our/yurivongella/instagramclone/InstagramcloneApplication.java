c7a85f2bdd80c71a6d03af744b55f8288d3dd370
DESKTOP-DS33HD0\Administrator <tor01237@gmail.com>
2021-02-21 01:18:33 +0900

    initailize commit

---
+++ src/main/java/our/yurivongella/instagramclone/InstagramcloneApplication.java
@@ -0,0 +1,13 @@
+package our.yurivongella.instagramclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/our/yurivongella/instagramclone/InstagramcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package our.yurivongella.instagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
