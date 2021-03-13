01807786eea206e1d0c5fbb52c4ca7840ef55a03
ParkJiwoon <bcp0109@gmail.com>
2021-03-14 03:46:05 +0900

    refactor: RESTful 형식에 맞춰서 API URI 형식 수정 (#45)
    - chore: Member 의 followings, follower mapping ID 가 반대로 되어 있어서 수정
    - refactor: RESTful 형식에 맞춰서 API URI 형식 수정

--- src/main/java/our/yurivongella/instagramclone/controller/AuthController.java
+++ src/main/java/our/yurivongella/instagramclone/controller/AuthController.java
@@ -1,5 +1,6 @@
 package our.yurivongella.instagramclone.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,18 +20,21 @@ import our.yurivongella.instagramclone.service.AuthService;
 public class AuthController {
     private final AuthService authService;
 
+    @ApiOperation("가입하기")
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody SignupRequestDto signupRequestDto) {
         String email = authService.signup(signupRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(email + " 회원가입이 완료되었습니다.");
     }
 
+    @ApiOperation("로그인")
     @PostMapping("/signin")
     public ResponseEntity<TokenDto> signin(@RequestBody SigninRequestDto signinRequestDto) {
         TokenDto tokenDto = authService.signin(signinRequestDto);
         return ResponseEntity.ok(tokenDto);
     }
 
+    @ApiOperation("로그인 토큰 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
