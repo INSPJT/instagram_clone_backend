5b6d02fdd03b024bd0e9cb645e62cbb6d5ba3602
DolphaGo <adamdoha@naver.com>
2021-03-28 05:30:38 +0900

    review

--- src/main/java/our/yurivongella/instagramclone/exception/ErrorCode.java
+++ src/main/java/our/yurivongella/instagramclone/exception/ErrorCode.java
@@ -15,8 +15,6 @@ public enum ErrorCode {
     INVALID_REFRESH_TOKEN(BAD_REQUEST, "리프레시 토큰이 유효하지 않습니다"),
     MISMATCH_REFRESH_TOKEN(BAD_REQUEST, "리프레시 토큰의 유저 정보가 일치하지 않습니다"),
     CANNOT_FOLLOW_MYSELF(BAD_REQUEST, "자기 자신은 팔로우 할 수 없습니다"),
-    ALREADY_LIKE(BAD_REQUEST, "더 이상 좋아요를 할 수 없습니다"),
-    ALREADY_UNLIKE(BAD_REQUEST, "더 이상 좋아요를 취소할 수 없습니다"),
 
     /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
     INVALID_AUTH_TOKEN(UNAUTHORIZED, "권한 정보가 없는 토큰입니다"),
@@ -27,9 +25,11 @@ public enum ErrorCode {
     REFRESH_TOKEN_NOT_FOUND(NOT_FOUND, "로그아웃 된 사용자입니다"),
     NOT_FOLLOW(NOT_FOUND, "팔로우 중이지 않습니다"),
     POST_NOT_FOUND(NOT_FOUND, "해당 포스트가 존재하지 않습니다."),
+    ALREADY_UNLIKE(NOT_FOUND, "더 이상 좋아요를 취소할 수 없습니다"),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),
+    ALREADY_LIKE(CONFLICT, "더 이상 좋아요를 할 수 없습니다"),
 
     /* 서버 내 데이터 오류 */
     INVALID_STATUS(INTERNAL_SERVER_ERROR, "서버 내 데이터에 오류가 있습니다");
