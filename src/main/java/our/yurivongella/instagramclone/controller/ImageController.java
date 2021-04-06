ed2d38103649c80e0939ede2a6a38573a64ebf5e
DolphaGo <adamdoha@naver.com>
2021-04-06 09:06:25 +0900

    S3 Upload

---
+++ src/main/java/our/yurivongella/instagramclone/controller/ImageController.java
@@ -0,0 +1,25 @@
+package our.yurivongella.instagramclone.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import our.yurivongella.instagramclone.service.S3Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class ImageController {
+    private final S3Service s3Service;
+
+    @PostMapping("/images")
+    public ResponseEntity<String> execWrite(MultipartFile file) throws IOException {
+        String imgPath = s3Service.upload(file);
+        return ResponseEntity.ok(imgPath);
+    }
+}
---
+++ src/main/java/our/yurivongella/instagramclone/service/S3Service.java
@@ -0,0 +1,50 @@
+package our.yurivongella.instagramclone.service;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.NoArgsConstructor;
+
+@Service
+@NoArgsConstructor
+public class S3Service {
+    private AmazonS3 s3Client;
+    @Value("${aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        s3Client = AmazonS3ClientBuilder.standard()
+                                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                                        .withRegion(this.region)
+                                        .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+                                   .withCannedAcl(CannedAccessControlList.PublicRead)); // 외부에 공개할 이미지이므로, 해당 파일에 public read 권한을 추가
+        return s3Client.getUrl(bucket, fileName).toString();
+    }
+}
