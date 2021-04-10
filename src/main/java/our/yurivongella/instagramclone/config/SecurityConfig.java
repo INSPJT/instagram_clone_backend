368f3fdb4f104749300c221ef801c3338017d10c
DolphaGo <adamdoha@naver.com>
2021-04-10 22:23:33 +0900

    edit Security Path

--- src/main/java/our/yurivongella/instagramclone/config/SecurityConfig.java
+++ src/main/java/our/yurivongella/instagramclone/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .authorizeRequests()
             .antMatchers("/auth/**").permitAll()
-            .antMatchers("/test/**").permitAll()
+            .antMatchers("/images/**").permitAll()
             .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
             // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
