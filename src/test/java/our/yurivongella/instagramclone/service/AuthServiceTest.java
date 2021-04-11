29df469a31d9895af13686f7dc86ee063423b55f
DolphaGo <adamdoha@naver.com>
2021-04-11 15:25:03 +0900

    edit

--- src/test/java/our/yurivongella/instagramclone/service/AuthServiceTest.java
+++ src/test/java/our/yurivongella/instagramclone/service/AuthServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import our.yurivongella.instagramclone.controller.dto.SigninRequestDto;
