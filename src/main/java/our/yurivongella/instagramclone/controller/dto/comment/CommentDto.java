2c6b123a005977e25e690eb68f724676ba18b348
ParkJiwoon <bcp0109@gmail.com>
2021-06-24 00:57:59 +0900

    feat: CommentDto 의 필드 MemberDto 를 MemberResDto 로 변경

--- src/main/java/our/yurivongella/instagramclone/controller/dto/comment/CommentDto.java
+++ src/main/java/our/yurivongella/instagramclone/controller/dto/comment/CommentDto.java
@@ -1,60 +1,38 @@
 package our.yurivongella.instagramclone.controller.dto.comment;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.ToString;
-import our.yurivongella.instagramclone.controller.dto.member.MemberDto;
-import our.yurivongella.instagramclone.entity.Member;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
+import our.yurivongella.instagramclone.controller.dto.member.MemberResDto;
 import our.yurivongella.instagramclone.entity.Comment;
 
 import java.time.LocalDateTime;
 
 @ToString
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class CommentDto {
-
     private Long id;
-
     private String content;
-
-    private MemberDto author;
-
+    private MemberResDto author;
     private Boolean isLike;
-
-    @JsonProperty("likeLength")
     private Long likeCount;
+    private Integer nestedCommentCount;
+    private LocalDateTime createdDate;
 
-    private Integer replyLength;
-
-    private LocalDateTime created;
-
-    @Builder
-    public CommentDto(final Long id, final String content, final MemberDto author, final Boolean isLike, final Long likeCount, final Integer replyLength,
-                      final LocalDateTime created) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-        this.isLike = isLike;
-        this.likeCount = likeCount;
-        this.replyLength = replyLength;
-        this.created = created;
+    public static CommentDto of(Comment comment) {
+        return CommentDto.builder()
+                         .id(comment.getId())
+                         .content(comment.getContent())
+                         .author(MemberResDto.of(comment.getMember()))
+                         .isLike(false)
+                         .likeCount(comment.getLikeCount())
+                         .nestedCommentCount(comment.getNestedComments().size())
+                         .createdDate(comment.getCreatedDate())
+                         .build();
     }
 
-    public static CommentDto of(Comment comment, Member member) {
-        return builder()
-                .id(comment.getId())
-                .content(comment.getContent())
-                .author(MemberDto.of(comment.getMember(), member))
-                .isLike(comment.getCommentLikes().stream().anyMatch(v -> v.getMember().getId().equals(member.getId())))
-                .likeCount(comment.getLikeCount())
-                .replyLength(comment.getNestedComments().size())
-                .created(comment.getCreatedDate())
-                .build();
+    public void setLikeTrue() {
+        this.isLike = true;
     }
 }
